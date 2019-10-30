7c9595317cf97ea6705d533f92acd2c107cad272
Logan Cleary <clearyl@gilmour.org>
2019-10-30 14:41:07 -0400

    obstacle counter

--- student.py
+++ student.py
@@ -120,13 +120,33 @@ class Piggy(PiggyParent):
             self.scan_data[angle] = self.read_distance()
 
     def obstacle_count(self):
-        print("I can't count how many obstacles are around me. Please give my programmer a zero.")
+        """Does a 360 scan and returns the number of obstacles it sees"""
+        found_something = False # trigger
+        trigger_distance = 250 
+        count = 0
+        starting_position = self.get_heading()
+        self.right(primary=60, counter=-60)
+        while self.get_heading() != starting_position:
+            if self.read_distance() < 250 and not found_something
+                found_something = True
+                count += 1
+                print("\n FOUND SOMETHING \n")
+            elif self.read_distance() > 250 and found_something:
+                found_something = False
+                print("I have a cleary view. Resetting my counter")
+        self.stop()
+        print("I found this many things: %d" % count)
+        return count 
+
 
     def nav(self):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
-        print("Wait a second. \nI can't navigate the maze at all. Please give my programmer a zero.")
+        while self.read_distance() > 250:
+            self.fwd()
+            time.sleep(.01)
+        self.stop()
 
 
 
