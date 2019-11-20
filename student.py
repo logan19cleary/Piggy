c568d5cd5f19b54fb60c1b92a514818f2100a11f
Logan Cleary <clearyl@gilmour.org>
2019-11-20 09:29:56 -0500

    read distance

--- student.py
+++ student.py
@@ -144,7 +144,7 @@ class Piggy(PiggyParent):
                 
         while True:    
             self.servo(self.MIDPOINT)
-            while self.quick_check():
+            while self.read_distance():
                 
                 self.fwd()
                 time.sleep(.01)
